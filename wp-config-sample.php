4d057b87500baf3932c1a8de326b69a38c70cf2d
Vaidhy Nathan <vaidhy.css.gdocs@gmail.com>
2017-06-05 08:47:29 +0000

    New commit

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'demo_database');
+define('DB_NAME', 'DATABASENAME');
 
 /** MySQL database username */
 define('DB_USER', 'DBUSER');
